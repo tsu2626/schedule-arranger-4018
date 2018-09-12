ad6611d7799de7e7f2aeb5966e12a9a7435b19ce
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-09-12 22:36:02 +0000

    create schedule-arranger

---
+++ routes/schedules.js
@@ -0,0 +1,35 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const authenticationEnsurer = require('./authentication-ensurer');
+const uuid = require('node-uuid');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
+
+
+router.get('/new', authenticationEnsurer, (req, res, next) => {
+  res.render('new', { user: req.user });
+});
+
+router.post('/', authenticationEnsurer, (req, res, next) => {
+  const scheduleId = uuid.v4();
+  const updatedAt = new Date();
+  Schedule.create({
+    scheduleId: scheduleId,
+    scheduleName: req.body.scheduleName.slice(0, 255),
+    memo: req.body.memo,
+    createdBy: req.user.id,
+    updatedAt: updatedAt
+  }).then((schedule) => {
+    const candidateNames = req.body.candidates.trim().split('\n').map((s) => s.trim()).filter((s) => s !== "");
+    const candidates = candidateNames.map((c) => { return {
+      candidateName: c,
+      scheduleId: schedule.scheduleId
+    };});
+    Candidate.bulkCreate(candidates).then(() => {
+      res.redirect('/schedules/' + schedule.scheduleId);
+    });
+  });
+});
+
+module.exports = router;
